72ca30e04de5feff2c80017b04d7ed25b4b180d9
jansc <janschab@gmail.com>
2020-01-05 17:17:12 +0100

    reszta

--- js/main.js
+++ js/main.js
@@ -1,7 +1,7 @@
 $(document).ready(() => {
-    const pola = ['dzien', 'termin', 'typ', 'przedmiot', 'sala'];
+    const pola = ['dzien', 'termin', 'typ', 'przedmiot', 'sala', 'od-godz', 'do-godz'];
 
-    const Json = $.getJSON('/json/plan.json', (json) => {
+    const Json = $.getJSON('./json/plan.json', (json) => {
         const template = $('.zajecie').first().clone().removeClass('d-none');
         $('#plan').empty();
 
@@ -11,16 +11,29 @@ $(document).ready(() => {
                 pola.forEach(pole => {
                     tpl.find(`.${pole}`).text(zajecie[pole]);
                 });
+                tpl.find('.przedmiot').data('tr', zajecie.nazwa);
+
                 if (zajecie.hasOwnProperty('kolokwium')) {
                     tpl.css({
                         backgroundColor: "#ff786e"
                     });
+                    tpl.find('.zakres').text(`Zakres materiału: ${zajecie['kolokwium']['zakres']}`)
+                } else {
+                    tpl.find('.info').empty();
                 }
-
-                tpl.data(zajecie);
 
                 tpl.appendTo('#plan');
             }
+        });
+
+        $('body').on('mouseenter mouseleave', '.zajecie', function() {
+            $(this).find('.info').stop().slideToggle('300', 'linear');
+        });
+
+        $('body').on('click', '.przedmiot', function() {
+            const targetTr = $(this).data().tr;
+            $(this).data('tr', $(this).text());
+            $(this).text(targetTr);
         });
     });
 });
